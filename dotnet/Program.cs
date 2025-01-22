794e6737e614cf75a98296f784f067ce11ab0450
Arsh Chauhan <ac2529@gmail.com>
2025-01-22 15:39:49 +0530

    Feat: Add a Dynamic Endpoint for Testing

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/info",async() =>
+{
+    string filePath = "info_file.txt";
+    return Results.Content(await File.ReadAllTextAsync(filePath));
+});
 
 app.Run();
