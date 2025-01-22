3c168099c54f56a113ff6f5117e73429e9a0f9d3
Arsh Chauhan <ac2529@gmail.com>
2025-01-22 17:22:06 +0530

    feat: Error Handling for info route

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -5,7 +5,16 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/info",async() =>
 {
     string filePath = "info_file.txt";
-    return Results.Content(await File.ReadAllTextAsync(filePath));
+    string content;
+    try
+    {
+        content = await File.ReadAllTextAsync(filePath);
+    }
+    catch (FileNotFoundException e)
+    {
+        return Results.StatusCode(500);
+    }
+    return Results.Content(content);
 });
 
 app.Run();
